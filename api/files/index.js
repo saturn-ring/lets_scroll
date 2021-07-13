de18bbc512ac0fb4aca08c22b888b493b7928250
Lano <69957752+Lano0423@users.noreply.github.com>
2021-07-13 19:06:34 +0900

    Update index.js

--- api/files/index.js
+++ api/files/index.js
@@ -1,5 +1,5 @@
 document.getElementById("btn").onclick=function(){
-    var number=document.getElementById("getnum").value;
+    var number=document.getElementById("num").value;
     var a=location.href.split("/");
     if(a.length!==1) a.length--;
     location.href="/view.html?number="+encodeURIComponent(number);  
