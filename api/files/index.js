6668ce18dcd0fffdef12520fadc79d038b55ecc6
Lano <69957752+Lano0423@users.noreply.github.com>
2021-07-13 19:38:57 +0900

    Update index.js

--- api/files/index.js
+++ api/files/index.js
@@ -1,4 +1,4 @@
 document.getElementById("btn").onclick = function() {
-    let num = document.getElementById("num").value || false;
+    let num = document.getElementById("num").value || true;
     if (!isNaN(num)) location.href = `/view.html?number=${num}`;
 }
