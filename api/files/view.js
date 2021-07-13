ca4aefdc185e0d0fd9b0c4cf974049b43639dc9c
Lano <69957752+Lano0423@users.noreply.github.com>
2021-07-13 19:41:24 +0900

    Update view.js

--- api/files/view.js
+++ api/files/view.js
@@ -1,16 +1,15 @@
 (async function () {
     try {
-            let params = new URLSearchParams(location.search.substring(1))
-            var list = await axios.get(`/list/${params.get('number')}`);
-            list=list.data;
+        let params = new URLSearchParams(location.search.slice(1));
+        var { data } = await axios.get(`/list/${params.get("number")}`);
     }
     catch (e) {
             document.write("없는 동인지거나 서버가 이상합니다");
     }
-    const maindiv = document.getElementById("images");
-    for (let i = 0; i < list.length; i++) {
+    const main = document.getElementById("images");
+    for (let i = 0; i < data.length; i++) {
             let d = document.createElement("img");
-            maindiv.appendChild(d);
-            d.src = `/image?url=${encodeURIComponent(list[i])}`;
+            main.appendChild(d);
+            d.src = `/image?url=${encodeURI(data[i])}`;
     }
 })();
