14579beb2676e6fcfce0aab6c5eb330731460c6f
darkapplepower <chictaehee@naver.com>
2021-07-13 17:41:35 +0900

    add avif ,webp option

--- api/hitomi.js
+++ api/hitomi.js
@@ -7,10 +7,13 @@ async function getList(num) {
 }
 
 function getLink(file) {
-    let { hash, name } = file, ret = "b";
-    let ext = name.split(".").pop();
+    let { hash, name } = file, ret = "b", dir;
+    if (file['hasavif']) {dir='avif';ret='a';}
+    if (file['haswebp']) {dir='webp';ret='a';}
+    let ext = dir || name.split(".").pop();
+    dir = dir || 'images';
     if (hash[2]) hash = hash.replace(/^.*(..)(.)$/, "$2/$1/" + hash);
-    let url = `https://a.hitomi.la/images/${hash}.${ext}`;
+    let url = `https://a.hitomi.la/${dir}/${hash}.${ext}`;
     let m = /\/[0-9a-f]\/([0-9a-f]{2})\//.exec(url);
     if (!m) return url.replace(/\/\/..?\.hitomi\.la/, "//a.hitomi.la");
     let g = parseInt(m[1], 16);
@@ -18,7 +21,7 @@ function getLink(file) {
         let o = 0;
         if (g < 0x80) o = 1;
         if (g < 0x40) o = 2;
-        ret = String.fromCharCode(97 + o) + "b";
+        ret = String.fromCharCode(97 + o) + ret;
     }
     return url.replace(/\/\/..?\.hitomi\.la/, `//${ret}.hitomi.la`);
 }
