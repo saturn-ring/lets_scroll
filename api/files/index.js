2a489ac08db82c81a3a25c6278b07b9034533a9c
darkapplepower <chictaehee@naver.com>
2021-07-16 13:54:02 +0900

    add onclick and enter key event

--- api/files/index.js
+++ api/files/index.js
@@ -1,4 +1,10 @@
-document.getElementById("btn").onclick = function() {
-    let num = document.getElementById("num").value || true;
-    if (!isNaN(num)) location.href = `/view.html?number=${num}`;
+document.getElementById("getnum").addEventListener("keydown", function (event) {
+        if (event.keyCode === 13) {
+                let num = document.getElementById("getnum").value || undefined;
+                if (!isNaN(num)) location.href = `/view.html?number=${num}`;
+        }
+})
+document.getElementById("btn").onclick = function () {
+        let num = document.getElementById("getnum").value || undefined;
+        if (!isNaN(num)) location.href = `/view.html?number=${num}`;
 }
