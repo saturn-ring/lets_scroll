a48a90fd60a6f027ebb3351d6dde6f3c3c874216
Lano <69957752+Lano0423@users.noreply.github.com>
2021-07-18 01:04:20 +0900

    Update selfCheck.js

--- api/selfCheck.js
+++ api/selfCheck.js
@@ -3,13 +3,6 @@ const crypto = new (require("node-jsencrypt"))();
 
 crypto.setPublicKey("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA81dCnCKt0NVH7j5Oh2+SGgEU0aqi5u6sYXemouJWXOlZO3jqDsHYM1qfEjVvCOmeoMNFXYSXdNhflU7mjWP8jWUmkYIQ8o3FGqMzsMTNxr+bAp0cULWu9eYmycjJwWIxxB7vUwvpEUNicgW7v5nCwmF5HS33Hmn7yDzcfjfBs99K5xJEppHG0qc+q3YXxxPpwZNIRFn0Wtxt0Muh1U8avvWyw03uQ/wMBnzhwUC8T4G5NclLEWzOQExbQ4oDlZBv8BM/WxxuOyu0I8bDUDdutJOfREYRZBlazFHvRKNNQQD2qDfjRz484uFs7b5nykjaMB9k/EJAuHjJzGs9MMMWtQIDAQAB");
 
-const path = [
-    "/v2/findUser",
-    "/v2/validatePassword",
-    "/v2/selectUserGroup",
-    "/registerServey"
-];
-
 const region = {
     "서울": "https://senhcs.eduro.go.kr",
     "부산": "https://penhcs.eduro.go.kr",
@@ -28,6 +21,12 @@ const region = {
     "경북": "https://gbehcs.eduro.go.kr",
     "경남": "https://gnehcs.eduro.go.kr",
     "제주": "https://jjehcs.eduro.go.kr",
+    "path": [
+        "/v2/findUser",
+        "/v2/validatePassword",
+        "/v2/selectUserGroup",
+        "/registerServey"
+    ]
 };
 
 async function find(name) {
@@ -37,23 +36,18 @@ async function find(name) {
 }
 
 async function getToken(json) {
-    let url = region[json.area] + path[0];
-    let code = await find(json.school);
-    let { data } = await axios({
-        "method": "POST",
-        "url": url,
-        "data": {
-            "orgCode": code,
-            "name": crypto.encrypt(json.name),
-            "birthday": crypto.encrypt(json.birth),
-            "loginType": "school"
-        }
+    let url = region[json.area] + region.path[0];
+    let { data } = await axios.post(url, {
+        "orgCode": await find(json.school),
+        "name": crypto.encrypt(json.name),
+        "birthday": crypto.encrypt(json.birth),
+        "loginType": "school"
     });
     return data.token;
 }
 
 async function getToken2(json) {
-    let url = region[json.area] + path[1];
+    let url = region[json.area] + region.path[1];
     let token = await getToken(json);
     let pass = crypto.encrypt(json.pass);
     let { data } = await axios({
@@ -66,18 +60,18 @@ async function getToken2(json) {
 }
 
 async function getToken3(json) {
-    let url = region[json.area] + path[2];
+    let url = region[json.area] + region.path[2];
     let token = await getToken2(json);
     let { data } = await axios({
         "method": "POST",
         "url": url,
         "headers": { "Authorization": token }
-    });
+    })
     return data[0].token;
 }
 
 module.exports = async function(json) {
-    let url = region[json.area] + path[3];
+    let url = region[json.area] + region.path[3];
     let token = await getToken3(json);
     let { data } = await axios({
         "method": "POST",
@@ -85,12 +79,11 @@ module.exports = async function(json) {
         "data": {
             "rspns01": "1",
             "rspns02": "1",
-            "rspns09": "0",
             "rspns00": "Y",
             "upperToken": token,
             "upperUserNameEncpt": "DAMI"
         },
         "headers": { "Authorization": token }
-    });
+    })
     return data;
 }
