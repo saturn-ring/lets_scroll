66dfdbb99e95022d2be6dfe027f24305582627bd
yukyum06 <69957752+Lano0423@users.noreply.github.com>
2021-12-26 23:32:48 +0900

    Delete hitomi-server directory

--- hitomi-server/files/index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("btn").onclick=function(){
-        var number=document.getElementById("getnum").value;
-        var a=location.href.split("/");
-        if(a.length!==1) a.length--;
-        location.href="/view.html?number="+encodeURIComponent(number);  
-}
