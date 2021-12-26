83b6529a1074b4ef3fa891fd3eed3852828015a7
다다미 <me@dadami.tech>
2021-12-26 15:24:02 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,6 @@
 const { get } = require("axios");
 const express = require("express");
 const hitomi = require("./hitomi");
-const selfCheck = require("./selfCheck");
-const getWeather = require("./weather");
-const comci = require("./comci");
 const app = express();
 const fs=require('fs');
 app.use(express.static("files"));
@@ -18,25 +15,6 @@ app.get("/image", function(req, res) {
         headers: { referer: "https://hitomi.la" }
     }).then(e => e.data.pipe(res));
 });
-
-app.get("/weather", async function(req, res) {
-    res.type("image/jpeg");
-    let url = await getWeather(req.query.area);
-    if (url) get(url, {"responseType": "stream"})
-        .then(e => e.data.pipe(res));
-    else  {
-        const r = fs.createReadStream('./weathererror.png');
-        r.pipe(res);
-    }
-})
-
-app.get("/selfCheck", async function(req, res) {
-    let query = ["name","birth","pass","school","area"];
-    if (!query.every(v => req.query[v]))
-        return res.json({error: "Invaild Arguments"});
-    selfCheck(req.query).then(v => res.json(v))
-        .catch(() => res.json({error: "request Failed"}));
-})
 
 app.get("/pixiv/:id", function(req, res) {
     let url = "http://pixiv.net/ajax/illust";
@@ -55,13 +33,6 @@ app.get("/comci/search", function(req, res){
                 res.json(x);
         });
 });
-app.get("/comci/timetable", function(req, res){
-        let {id, g, c}=req.query;
-        comci.getTimeTable(id, g, c).then(x=>{
-                res.json(x);
-        }).catch(x=>{
-                res.json({"error": x.toString()});
-        })
-});
+
 app.get("*", (_, res) => res.sendFile(`${__dirname}/404.html`));
 app.listen(3000, () => console.log("API Enabled At port 3000\n"));
