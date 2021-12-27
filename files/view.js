774d3b2ec0c3e6905240b813d651136cb1a2f48c
P0dStellar <78299655+P0dStellar@users.noreply.github.com>
2021-12-27 21:45:02 +0900

    view.html에 맞게 수정
    view.html에 해당 스크립트가 포함돼 있습니다.
    (스크립트가 링크돼 있지 않음)

--- files/view.js
+++ files/view.js
@@ -1,14 +1,14 @@
 (async function() {
-        fetch(`/link${location.search}`)
-        .then(response => response.json())
-        .then(response => {
-                if (response.length >= 1) {
-                        const main = document.getElementById("images");
-                        for (const s of response) {
-                                let el = document.createElement("img");
-                                main.append(el); el.src = `/img?url=${s}`;
-                        }
-                }
-                else document.write("ERROR");
-        });
+fetch(`/link${location.search}`)
+.then(response => response.json())
+.then(response => {
+    if (response.length >= 1) {
+        const main = document.getElementsByClassName("img");
+        for (const s of response) {
+                let el = document.createElement("img");
+                main.append(el); el.src = `/img?url=${s}`;
+        }
+    }
+    else document.write("ERROR");
+});
 })();
