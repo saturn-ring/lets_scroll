ab3e34026cae9ec73a959da47305e7a2ee9bf9b5
P0dStellar <78299655+P0dStellar@users.noreply.github.com>
2021-12-28 16:17:09 +0900

    로드되지 않는 이미지 삭제

--- files/view.js
+++ files/view.js
@@ -1,12 +1,13 @@
-
 fetch(`/link${location.search}`)
 .then(response => response.json())
 .then(response => {
     if (response.length >= 1) {
         const main = document.getElementById("image");
         for (const s of response) {
-                let el = document.createElement("img");
-                main.append(el); el.src = `/img?url=${s}`;
+            let el = document.createElement("img");
+            main.append(el);
+            el.src = `/img?url=${s}`;
+            el.onerror = "this.parentNode.style.display='none'";
         }
     }
     else document.write("ERROR");
