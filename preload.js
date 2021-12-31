edc4ec4406c7cdf758b562612d70e98f587acd14
franknoh <franknoh1218@naver.com>
2021-12-31 14:22:00 +0900

    ㅇㅇ

--- preload.js
+++ preload.js
@@ -1,13 +1,15 @@
 const customTitlebar = require('custom-electron-titlebar');
 
 window.addEventListener('DOMContentLoaded', () => {
-    const version = document.createElement('div');
-    version.innerText = `v${require('./package.json').version}`;
-    version.style.position = 'absolute';
-    version.style.right = '5px';
-    version.style.bottom = '5px';
-    version.style.color = '#82828282';
-    document.getElementsByClassName('wrp')[0].appendChild(version);
+    if(window.location.href.endsWith("index.html")){
+        const version = document.createElement('div');
+        version.innerText = `v${require('./package.json').version}`;
+        version.style.position = 'absolute';
+        version.style.right = '5px';
+        version.style.bottom = '5px';
+        version.style.color = '#82828282';
+        document.getElementsByClassName('wrp')[0].appendChild(version);
+    }
     titlebar = new customTitlebar.Titlebar({
     	backgroundColor: customTitlebar.Color.fromHex('#191919'),
         menu: null
